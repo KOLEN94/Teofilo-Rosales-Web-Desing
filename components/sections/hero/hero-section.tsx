bcbde3f4b6a0f214da6efbec0b4187b20c4afffe
Teofilo <breaknius@gmail.com>
2026-07-14 11:04:31 -0500

    Actualizacion-14-julio-2026

--- components/sections/hero/hero-section.tsx
+++ components/sections/hero/hero-section.tsx
@@ -59,7 +59,7 @@ export default function HeroSection() {
               size="lg"
               className="text-md bg-transparent text-white border-white hover:bg-white/10 hover:text-white"
             >
-              <a href="https://drive.google.com/file/d/1ptffebJJz50wX_BoeUnfp7w3vRqJlKz8/view?usp=sharing" target="_blank">Cv Español</a>
+              <a href="https://drive.google.com/file/d/1pLko-Wvw6YId8lzOJZ3zaufVBzqlUwaI/view?usp=sharing" target="_blank">Cv Español</a>
             </Button>
             <Button
               asChild
@@ -67,7 +67,7 @@ export default function HeroSection() {
               size="lg"
               className="text-md bg-transparent text-white border-white hover:bg-white/10 hover:text-white"
             >
-              <a href="https://drive.google.com/file/d/1cCYbgb9z0C9-2bSH1gLzfdr5OhD4uJf7/view?usp=sharing" target="_blank">Cv English</a>
+              <a href="https://drive.google.com/file/d/1Qf8Xv7PfarVPhv-tBKmn3u3_FhLjuH84/view?usp=sharing" target="_blank">Cv English</a>
             </Button>
           </motion.div>
 
