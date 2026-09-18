0365db6f722c875185f8d46ab66d5170733f147e
Teofilo <breaknius@gmail.com>
2026-09-18 18:15:18 -0500

    Actualizacion 18-setiembre-2026

--- data/testimonials.ts
+++ data/testimonials.ts
@@ -7,16 +7,26 @@ export type Testimonial = {
 }
 
 export const testimonials: Testimonial[] = [
-  {
+    {
     id: 1,
+    name: "Miguel Flores",
+    position: "Fundador de Frontera Militar",
+    image: "/placeholder.svg?height=100&width=100",
+    quote:
+      "Teófilo es un profesional con una excelente capacidad de planificación y una gran disposición para atender y resolver cualquier duda durante el desarrollo de mi proyecto. La comunicación fue muy efectiva desde el inicio hasta la culminación del trabajo, lo que permitió llevar adelante cada etapa de manera ordenada y eficiente. Sin duda, volveré a trabajar con él en futuros proyectos.",
+  },
+
+  {
+    id: 2,
     name: "Patricio Zaltzberg",
     position: "Executive and Commercial Director UPA Enterteinament",
     image: "/placeholder.svg?height=100&width=100",
     quote:
       "Excelente trabajo. Supo entender perfectamente lo que necesitábamos y lo resolvió con rapidez, claridad y mucha dedicación. ¡Recomendado 100%!.",
   },
+  
   {
-    id: 1,
+    id: 3,
     name: "Karla M",
     position: "Direct Marketing Executive",
     image: "/placeholder.svg?height=100&width=100",
@@ -24,7 +34,7 @@ export const testimonials: Testimonial[] = [
       "Teofilo hizo un gran trabajo, lo entregó a tiempo y la comunicación con él fue muy buena. Entendió perfectamente los requisitos del trabajo y lo llevó a cabo a la perfección. Hizo las correcciones necesarias dentro del presupuesto. Definitivamente volvería a contar con él para más trabajos. Lo recomiendo ampliamente.",
   },
   {
-    id: 2,
+    id: 4,
     name: "Tecnotecnicae",
     position: "Customer Directives Strategist",
     image: "/placeholder.svg?height=100&width=100",
@@ -32,7 +42,7 @@ export const testimonials: Testimonial[] = [
       "Trabajar con Teofilo ha sido muy sencillo, ha entendido a la perfección el trabajo y lo ha ejecutado de forma correcta y a tiempo. La comunicación ha sido fluida y el resultado final tal y como se requirió. Lo recomiendo.",
   },
   {
-    id: 3,
+    id: 5,
     name: "Joe Vargas",
     position: "Forward Intranet Liaison",
     image: "/placeholder.svg?height=100&width=100",
@@ -40,7 +50,7 @@ export const testimonials: Testimonial[] = [
       "Contraté a Teofilo para la migración y mejora de dos páginas web que tengo. Fue muy serio con su trabajo, siempre estuvo pendiente de mis consultas y dudas. Seguiré trabajando con él y además lo recomiendo.",
   },
   {
-    id: 4,
+    id: 6,
     name: "Acronia Studio",
     position: "Direct Group Designer",
     image: "/placeholder.svg?height=100&width=100",
@@ -48,15 +58,15 @@ export const testimonials: Testimonial[] = [
       "Trabajador muy comprometido, trabajó constantemente en el proyecto hasta que lo finalizó con éxito.",
   },
   {
-    id: 5,
+    id: 7,
     name: "Nick Fiverr - Emprendedor2016",
     position: "Forward Solutions Agent",
     image: "/placeholder.svg?height=100&width=100",
     quote:
       "Es un profesional, ha hecho un trabajo fantástico, rápido y creativo para mejorar la web y obtener mejores resultados. Lo recomiendo 100%.",
   },
-    {
-    id: 6,
+      {
+    id: 8,
     name: "Daniel IT",
     position: "Forward Solutions Agent",
     image: "/placeholder.svg?height=100&width=100",
@@ -64,7 +74,7 @@ export const testimonials: Testimonial[] = [
       "EH Hola Teofilo, de mi parte me queda decir gracias. Contigo realicé los puntos pendientes en mi página web y estoy satisfecho con tu trabajo. Es muy probable que en un futuro salgan más trabajos y espero contar contigo en ellos. Saludos.",
   },
       {
-    id: 7,
+    id: 9,
     name: "Betty Ramos",
     position: "Dueña Imaginemos, Tienda de juguetes para niños",
     image: "/placeholder.svg?height=100&width=100",
